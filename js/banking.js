0d321c969030f3f1b6110667b650058bd38f2abd
suronjit797 <suronjit797@gmail.com>
2022-02-11 00:11:20 +0600

    function

--- js/banking.js
+++ js/banking.js
@@ -1,35 +1,37 @@
-let depositValue = document.getElementById('deposit_value')
-let withdrawValue = document.getElementById('withdraw_value')
-let balanceValue = document.getElementById('balance_value')
-let addDepo = document.getElementById('addDepo')
-let addDepo_btn = document.getElementById('addDepo_btn')
-let addWith = document.getElementById('addWith')
-let addWith_btn = document.getElementById('addWith_btn')
+// all
+function takeInput(inputId, outputId, isAdd) {
+    const getElement = document.getElementById(inputId)
+    let balance = document.getElementById('balance_value')
+    balanceValue = parseFloat(balance.innerText)
+    let getElementValue = parseFloat(getElement.value) > 0 ? parseFloat(getElement.value) : 0
+    if (!isAdd && balanceValue < getElementValue) {
+        getElementValue = 0
+        console.log('you haven\'t enough money')
+    } 
 
 
+    // set output
+    let outputValueText = document.getElementById(outputId)
+    let outputValue = parseFloat(outputValueText.innerText)
+    outputValueText.innerText = outputValue + getElementValue
+    // balance update
+    if (isAdd) {
+        let totalValue = balanceValue + getElementValue
+        balance.innerText = totalValue
+    } else {
+        let totalValue = balanceValue - getElementValue
+        balance.innerText = totalValue
+    }
+    return outputValue
+}
 
-addDepo_btn.addEventListener('click', (e) => {
-    let depositValueNumber = parseFloat(depositValue.innerText)
-    let depositInput = parseFloat(addDepo.value)
-    // deposit value change
-    depositValueNumber = depositValueNumber + depositInput
-    depositValue.innerText = depositValueNumber
-    // balance value change 
-    let balance = parseFloat(balanceValue.innerText)
-    let totalBalance = depositInput + balance
-    balanceValue.innerText = totalBalance
-    addDepo.value = ''
+// deposit
+document.getElementById('addDepo_btn').addEventListener('click', () => {
+    takeInput('addDepo', 'deposit_value', true)
 })
 
-addWith_btn.addEventListener('click', (e) => {
-    let withdrawValueNumber = parseFloat(withdrawValue.innerText)
-    let withdrawInput = parseFloat(addWith.value)
-    // deposit value change
-    withdrawValueNumber = withdrawValueNumber + withdrawInput
-    withdrawValue.innerText = withdrawValueNumber
-    // balance value change 
-    let balance = parseFloat(balanceValue.innerText)
-    let totalBalance = balance - withdrawInput
-    balanceValue.innerText = totalBalance
-    addWith.value = ''
+
+// withdraw
+document.getElementById('addWith_btn').addEventListener('click', () => {
+    takeInput('addWith', 'withdraw_value', false)
 })
